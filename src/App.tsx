bb0dcb1e45706b769962539f3ce2496c4019fd65
Ishan Dasgupta <ishan.dasguptar@gmail.com>
2022-06-07 16:24:02 -0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Layout, Menu } from 'antd';
+import React, { useState } from 'react';
+import './App.less';
+import ExplorePage from './components/ExplorePage';
 
-function App() {
+const { Header, Content, Footer } = Layout;
+
+const App: React.FC = () => {
+
+  const [currPage, setCurrPage] = useState(1);
+
+  const menuItems = [
+    { label: 'Explore', key: 'item-1' },
+    { label: 'Genres', key: 'item-2' },
+  ]
+
+  const onMenuItemClicked = (props : {item: any, key: String, keyPath: String[], domEvent: any}) => {
+    const {key} = props;
+
+    if(key === "item-1") {
+      setCurrPage(1);
+    } else {
+      setCurrPage(2);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout className="layout">
+      <Header style={{
+        backgroundColor: "white",
+      }}>
+        <div className="logo" />
+        <Menu
+          theme="light"
+          mode="horizontal"
+          defaultSelectedKeys={['1']}
+          items={menuItems}
+          onClick={onMenuItemClicked}
+        />
+      </Header>
+      <Content >
+        {currPage === 1 &&
+          <ExplorePage></ExplorePage>
+        }
+        {currPage === 2 &&
+          <div className="site-layout-content">Content Page 2</div>
+        }
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>TLDR ©2020 Created by Ishan Dasgupta and Aaditya Ganesan</Footer>
+    </Layout>
   );
 }
 
